415090d42bb7bb3f81ff4253f3095951ab4f341b
Saikat Gupta <saikat.gupta1975@gmail.com>
2020-12-25 18:33:26 +0530

    Initial commit

---
+++ src/main/java/com/saikat/housie/HousieApplication.java
@@ -0,0 +1,13 @@
+package com.saikat.housie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saikat/housie/HousieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saikat.housie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
